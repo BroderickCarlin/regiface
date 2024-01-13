4f9950e32750cb00d3a8fe28d095d29c9dc09afd
Broderick Carlin <broderick.carlin@gmail.com>
2024-01-13 11:08:02 -0600

    feat: Restructure the repo slightly, and get the traits close to their final forms

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,77 @@
-pub use from_slice::*;
-pub use into_slice::*;
+use ::embedded_hal::i2c::{AddressMode, I2c, Operation};
+pub use slice::*;
+use thiserror::Error;
 
-pub mod embedded_hal;
-mod from_slice;
-mod into_slice;
+mod slice;
+
+#[derive(Clone, Copy, Debug, Error)]
+pub enum ReadRegisterError<D, S> {
+    BusError(D),
+    DeserializationError(S),
+}
+
+#[derive(Clone, Copy, Debug, Error)]
+pub enum WriteRegisterError<D, S> {
+    BusError(D),
+    SerializationError(S),
+}
 
 /// The generic top level trait for all register values
 pub trait Register {
     fn id() -> u8;
 }
 
-/// A marker trait for registers that are readable
-pub trait ReadableRegister<const N: usize = 1>: Register + FromSlice<N> {}
+pub trait ReadableI2cRegister<const N: usize = 1>: Register + FromSlice<N> {
+    fn readable_id() -> u8 {
+        Self::id() | 0x40
+    }
 
-/// A marker trait for registers that are writable
-pub trait WritableRegister<const N: usize = 1>: Register + IntoSlice<N> {}
+    fn read<D, A>(
+        device: &mut D,
+        device_addr: A,
+    ) -> Result<Self, ReadRegisterError<D::Error, Self::Error>>
+    where
+        A: AddressMode,
+        D: I2c<A>,
+    {
+        let mut buf = [0u8; N];
+
+        device
+            .write_read(device_addr, &[Self::readable_id()], &mut buf)
+            .map_err(ReadRegisterError::BusError)?;
+
+        Self::from_slice(buf).map_err(ReadRegisterError::DeserializationError)
+    }
+}
+
+/// A trait for registers that are writable on an I2C device
+pub trait WritableI2cRegister<const N: usize = 1>: Register + IntoSlice<N> {
+    fn writeable_id() -> u8 {
+        Self::id()
+    }
+
+    fn write<D, A>(
+        self,
+        device: &mut D,
+        device_addr: A,
+    ) -> Result<(), WriteRegisterError<D::Error, Self::Error>>
+    where
+        Self: Sized,
+        A: AddressMode,
+        D: I2c<A>,
+    {
+        let buf = self
+            .into_slice()
+            .map_err(WriteRegisterError::SerializationError)?;
+
+        device
+            .transaction(
+                device_addr,
+                &mut [
+                    Operation::Write(&[Self::writeable_id()]),
+                    Operation::Write(&buf),
+                ],
+            )
+            .map_err(WriteRegisterError::BusError)
+    }
+}
