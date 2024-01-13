547ec42c3d7cb514419384bced03a2f01142cd17
Broderick Carlin <broderick.carlin@gmail.com>
2024-01-13 11:28:10 -0600

    feat: Switch to using PackedStruct for serde operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
 use ::embedded_hal::i2c::{AddressMode, I2c, Operation};
-pub use slice::*;
+use packed_struct::{types::bits::ByteArray as _, PackedStruct, PackingError};
 use thiserror::Error;
 
-mod slice;
-
 #[derive(Clone, Copy, Debug, Error)]
-pub enum ReadRegisterError<D, S> {
+pub enum ReadRegisterError<D> {
     BusError(D),
-    DeserializationError(S),
+    DeserializationError(PackingError),
 }
 
 #[derive(Clone, Copy, Debug, Error)]
-pub enum WriteRegisterError<D, S> {
+pub enum WriteRegisterError<D> {
     BusError(D),
-    SerializationError(S),
+    SerializationError(PackingError),
 }
 
 /// The generic top level trait for all register values
@@ -21,47 +19,48 @@ pub trait Register {
     fn id() -> u8;
 }
 
-pub trait ReadableI2cRegister<const N: usize = 1>: Register + FromSlice<N> {
+pub trait ReadableI2cRegister: Register + PackedStruct {
+    #[inline]
     fn readable_id() -> u8 {
         Self::id() | 0x40
     }
 
-    fn read<D, A>(
-        device: &mut D,
-        device_addr: A,
-    ) -> Result<Self, ReadRegisterError<D::Error, Self::Error>>
+    #[inline]
+    fn read<D, A>(device: &mut D, device_addr: A) -> Result<Self, ReadRegisterError<D::Error>>
     where
         A: AddressMode,
         D: I2c<A>,
     {
-        let mut buf = [0u8; N];
+        let mut buf = Self::ByteArray::new(0);
 
         device
-            .write_read(device_addr, &[Self::readable_id()], &mut buf)
+            .write_read(
+                device_addr,
+                &[Self::readable_id()],
+                buf.as_mut_bytes_slice(),
+            )
             .map_err(ReadRegisterError::BusError)?;
 
-        Self::from_slice(buf).map_err(ReadRegisterError::DeserializationError)
+        Self::unpack(&buf).map_err(ReadRegisterError::DeserializationError)
     }
 }
 
 /// A trait for registers that are writable on an I2C device
-pub trait WritableI2cRegister<const N: usize = 1>: Register + IntoSlice<N> {
+pub trait WritableI2cRegister: Register + PackedStruct {
+    #[inline]
     fn writeable_id() -> u8 {
         Self::id()
     }
 
-    fn write<D, A>(
-        self,
-        device: &mut D,
-        device_addr: A,
-    ) -> Result<(), WriteRegisterError<D::Error, Self::Error>>
+    #[inline]
+    fn write<D, A>(self, device: &mut D, device_addr: A) -> Result<(), WriteRegisterError<D::Error>>
     where
         Self: Sized,
         A: AddressMode,
         D: I2c<A>,
     {
         let buf = self
-            .into_slice()
+            .pack()
             .map_err(WriteRegisterError::SerializationError)?;
 
         device
@@ -69,7 +68,7 @@ pub trait WritableI2cRegister<const N: usize = 1>: Register + IntoSlice<N> {
                 device_addr,
                 &mut [
                     Operation::Write(&[Self::writeable_id()]),
-                    Operation::Write(&buf),
+                    Operation::Write(buf.as_bytes_slice()),
                 ],
             )
             .map_err(WriteRegisterError::BusError)
