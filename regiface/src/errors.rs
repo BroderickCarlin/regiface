47e5e747c8a0be81d0b2b6bab25a3c5a159f8d0c
Broderick Carlin <broderick.carlin@gmail.com>
2024-12-12 16:55:21 -0600

    fix: Remove ThisError dependancy

--- regiface/src/errors.rs
+++ regiface/src/errors.rs
@@ -1,18 +1,16 @@
-use thiserror::Error;
-
-#[derive(Clone, Copy, Debug, Error)]
+#[derive(Clone, Copy, Debug)]
 pub enum ReadRegisterError<B, D> {
     BusError(B),
     DeserializationError(D),
 }
 
-#[derive(Clone, Copy, Debug, Error)]
+#[derive(Clone, Copy, Debug)]
 pub enum WriteRegisterError<B, S> {
     BusError(B),
     SerializationError(S),
 }
 
-#[derive(Clone, Copy, Debug, Error)]
+#[derive(Clone, Copy, Debug)]
 pub enum CommandError<B, S, D> {
     BusError(B),
     SerializationError(S),
